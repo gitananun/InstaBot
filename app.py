655892afe8ed0ef7416b0e11e285dd74417a55ef
Tigran Muradyan <tigran.muradyan140201@gmail.com>
2020-06-09 12:19:43 +0400

    Deployable

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import time
 
-class TwitterBot:
+class InstaBot:
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -11,29 +10,30 @@ class TwitterBot:
 
     def login(self):
         bot = self.bot
-        bot.get('https://twitter.com/')
+        bot.get('https://instagram.com/')
         time.sleep(3)
-        email = bot.find_element_by_name('session[username_or_email]')
-        password = bot.find_element_by_name('session[password]')
+        email = bot.find_element_by_name('username')
+        password = bot.find_element_by_name('password')
         email.clear()
         password.clear()
         email.send_keys(self.username)
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
         time.sleep(10)
+        bot.find_element_by_class_name('y3zKF').click()
 
-    def scroll_reload(self, hashtag):
+    def search(self, input):
         bot = self.bot
-        search = bot.find_element_by_class_name('r-1swcuj1')
-        search.send_keys(hashtag)
-        search.send_keys(Keys.RETURN)
+        bot.get('https://www.instagram.com/explore/tags/'+input+'/')
         time.sleep(3)
-        for i in range(1,5):
-            bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-            time.sleep(5)
-        bot.execute_script('window.scrollTo(document.body.scrollHeight, 0)')
+        posts = bot.find_elements_by_class_name("eLAPa")
+        for post in posts:
+            post.click()
+            follow = bot.find_element_by_class_name('bY2yH')
+            follow.click()
+            time.sleep(4)
+            bot.find_element_by_class_name('TxciK').click()
 
-
-tigran = TwitterBot('tigran.muradyan140201@gmail.com', 'nartigani')
+tigran = InstaBot('usr', 'pwd')
 tigran.login()
-tigran.scroll_reload('webdevelopment')
+tigran.search('hayastan')
